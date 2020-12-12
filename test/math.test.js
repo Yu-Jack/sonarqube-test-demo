da2f839e811fcb49a577356bcfb99ceaeacfd4c7
Yu-Jack <jk82421@gmail.com>
2020-12-12 21:02:15 +0800

    add code

--- test/math.test.js
+++ test/math.test.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 const math = require("../math");
-describe('Array', function () {
+describe('Number', function () {
     it('should return sun when add two number ', function () {
         assert.deepStrictEqual(math.sum(1, 5), 6);
     });
